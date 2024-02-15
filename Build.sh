f00fa7e780040ad43f93074639f91c09653d6d9c
Dorian <orionthehyena@gmail.com>
2024-02-15 02:09:42 -0700

    made build.sh work

--- Build.sh
+++ Build.sh
@@ -2,8 +2,9 @@
 
 CLASSPATH="./lib/json-smart-2.5.0.jar:./lib/jsonrpc2-client-2.1.1.jar:./lib/jsonrpc2-base-2.1.1.jar:./lib/asm-1.0.2.jar"
 
-rm ./build/*.class
+rm ./build
 printf "Building..\n\n"
+mkdir build
 
 printf "Classpath: $CLASSPATH\n\n"
 printf "Compiling..\n\n"
