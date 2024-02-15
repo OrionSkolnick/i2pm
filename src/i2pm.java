0de23e6e2324dd7f5a0208bc86406ef73d3ccabc
Dorian <orionthehyena@gmail.com>
2024-02-15 02:30:48 -0700

    added a bit of comments

--- src/i2pm.java
+++ src/i2pm.java
@@ -10,28 +10,29 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-public class i2pm {
+public class i2pm { //main class, entry point in program
 
-	private JSONRPC2Session session;
-	private final String token;
+	private JSONRPC2Session session; //jsonrpc session with server
+	private final String token; //the token is used for authentication with the server
+				    //the password is given to the server, and THEN the token is returned
+				    //from there, token is used for all other authentication
 
-	public i2pm(URL url, String password) throws Exception {
+	public i2pm(URL url, String password) throws Exception { //constructor with lazy exception handling
+								 //TODO fix error handling
 		session = new JSONRPC2Session(url);
+		JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
 
+		//initializes and creates the request to get the token
 		String method = "Authenticate";
 		Map<String,Object> params = new HashMap<String,Object>();
 		params.put("API", 1);
 		params.put("Password", password);
 		String id = "1";
-		
 		JSONRPC2Request req = new JSONRPC2Request(method, params, id);
 
-		JSONRPC2Response response = this.getSession().send(req);
-
-		JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
-
-		JSONObject result = (JSONObject) parser.parse(response.getResult().toString());
-		token = (String)result.get("Token");
+		JSONRPC2Response response = this.getSession().send(req); // gets response from server
+		JSONObject result = (JSONObject) parser.parse(response.getResult().toString()); //gets json result field from response
+		token = (String)result.get("Token"); //sets token to value from result map
 	}
 
 	public JSONRPC2Session getSession() {
@@ -48,13 +49,15 @@ public class i2pm {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		if (args.length < 2) {
+		if (args.length < 2) { //makes sure there are two args
 			System.out.println("please put in args");
 			i2pm.printHelp();
 		}
 		String[] potentialURL = args[0].split("://", 2);
-		i2pm main = new i2pm(new URI(potentialURL[0], "//" + potentialURL[1], "").toURL(), args[1]);
+		i2pm main = new i2pm(new URI(potentialURL[0], "//" + potentialURL[1], "").toURL(), args[1]); //makes i2pm object from url passed and password
 
+		//HACK copied code and made redundant, just a test function to get uptime
+		//prepares request for uptime
 		String method = "RouterInfo";
 		Map<String,Object> params = new HashMap<String,Object>();
 		params.put("Token", main.getToken());
@@ -62,13 +65,12 @@ public class i2pm {
 		String id = "1";
 		
 		JSONRPC2Request req = new JSONRPC2Request(method, params, id);
+		JSONRPC2Response response = main.getSession().send(req); //gets from server
 
-		JSONRPC2Response response = main.getSession().send(req);
-
-		JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
+		JSONParser parser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE); //TODO make a universal parser? there was one in the constructor
 		JSONObject result = (JSONObject) parser.parse(response.getResult().toString());
 
-		System.out.println(String.format("Your i2p router has been up for: %02d min, %02d sec", 
+		System.out.println(String.format("Your i2p router has been up for: %02d min, %02d sec", //formats server result to human readable time
 			TimeUnit.MILLISECONDS.toMinutes((int)result.get("i2p.router.uptime")),
 			TimeUnit.MILLISECONDS.toSeconds((int)result.get("i2p.router.uptime")) - 
 			TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((int)result.get("i2p.router.uptime")))));
